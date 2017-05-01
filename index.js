128a44b7bbfcd60252721125623ef963cc1c50ed
Ben Chociej <ben@chociej.io>
2017-05-01 15:55:21 -0500

    remove lame

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 var fs = require('fs');
-var lame = require('lame');
 var path = require('path');
 var Speaker = require('speaker');
 
 module.exports = {
 	play: function() {
-		fs.createReadStream(path.resolve(__dirname, './trombone.mp3'))
-			.pipe(new lame.Decoder())
-			.on('format', function(format) {
-				this.pipe(new Speaker(format));
-			});
+		var format = {
+			raw_encoding: 208,
+			sampleRate: 44100,
+			channels: 2,
+			signed: true,
+			float: false,
+			ulaw: false,
+			alaw: false,
+			bitDepth: 16
+		};
+		var spk = new Speaker(format);
+		fs.createReadStream(path.resolve(__dirname, './trombone.pcm')).pipe(spk);
 	}
 };
